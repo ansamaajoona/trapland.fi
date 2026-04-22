92e4350d2750b25462bde1a13a50f6e4daed94f4
Joona Ansamaa <joona.ansa@gmail.com>
2026-04-22 17:42:27 +0300

    add vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import { Inter } from 'next/font/google';
 import './globals.css';
+import { Analytics } from '@vercel/analytics/next';
 
 const inter = Inter({
   variable: '--font-inter',
@@ -14,6 +15,7 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <body className={`${inter.variable} antialiased`}>{children}</body>
+      <Analytics />
     </html>
   );
 }
